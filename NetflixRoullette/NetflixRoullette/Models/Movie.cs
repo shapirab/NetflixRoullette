89b96f86e4931783ba1c7995a547cf2f0a69ad5a
shapirab <shapirab@jamd.ac.il>
2024-02-12 07:57:52 +0200

    Add project files.

---
+++ NetflixRoullette/NetflixRoullette/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetflixRoullette.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string CoverUrl { get; set; }
+        List<Actor> Actors { get; set; }
+    }
+}
