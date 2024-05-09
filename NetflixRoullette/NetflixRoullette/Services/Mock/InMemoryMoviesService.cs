2a8f058d232bc30c8d94522b4020fa33c90861dc
shapirab <shapirab@jamd.ac.il>
2024-05-09 09:20:30 +0300

    9/5/24 completed design of MovieDetailPage

--- NetflixRoullette/NetflixRoullette/Services/Mock/InMemoryMoviesService.cs
+++ NetflixRoullette/NetflixRoullette/Services/Mock/InMemoryMoviesService.cs
@@ -24,7 +24,11 @@ namespace NetflixRoullette.Services.Mock
                     Actors = new List<Actor>()
                     {
                         new Actor { Id = 2, FirstName = "Robin", LastName = "Williams"}
-                    }
+                    },
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris " +
+                    "nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in "
                 },
                 new Movie{ 
                     Id = 2, 
